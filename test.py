fba093c57f19b4dbba00707e03a6f1efdb2c8f4b
Shuby Deshpande <shubhankardeshpande@hotmail.com>
2019-06-21 16:25:22 -0700

    change default to evaluate on nuro data

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 import glob
 import argparse
-
+import ipdb
 # Kerasa / TensorFlow
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
 from keras.models import load_model
@@ -13,7 +13,7 @@ from matplotlib import pyplot as plt
 # Argument Parser
 parser = argparse.ArgumentParser(description='High Quality Monocular Depth Estimation via Transfer Learning')
 parser.add_argument('--model', default='nyu.h5', type=str, help='Trained Keras model file.')
-parser.add_argument('--input', default='examples/*.png', type=str, help='Input filename or folder.')
+parser.add_argument('--input', default='sample/images/*png', type=str, help='Input filename or folder.')
 args = parser.parse_args()
 
 # Custom object needed for inference and training
